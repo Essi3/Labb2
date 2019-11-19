75080725eeb58db3bce00d8bb785c10439a7d2d9
Essi3 <esroozen@live.se>
2019-11-19 12:30:00 +0100

    first commit

---
+++ Labb2/Position.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Labb2
+{
+    //Denna klass skall representera en position i ett tvådimensionellt koordinatsystem, alltså ett par av en x•koordinat och en y•koordinat
+    public class Position
+    {
+        private int x;
+        private int y;
+
+        //En position skall ha två stycken properties: X•värdet, Y•värdet. Bägge dessa properties ska kunna förändras, men får aldrig bli negativa.
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value >= 0 ? value : value * -1;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value >= 0 ? value : value * -1;
+            }
+        }
+
+        //returnera avståndet från origo (koordinaten (0, 0)) till den här punkten
+        public double Length()
+        {
+            var distanceFromOrigo = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            return distanceFromOrigo;
+        }
+
+        //konstruktor som tar emot ett x-värde och ett y-värde och lagrar dem sedan i lämpliga properties
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        //returnera true om den givna positionen är samma som den här ■ Annars skall den returnera false. Två punkter räknas som lika om både X•koordinaten och Y•koordinaten är lika
+        public bool Equals(Position p)
+        {
+            if (p.X.Equals(X) && p.Y.Equals(Y))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //returnera en ny instans av Position, som har samma X och Y•värden som den nuvarande punkten
+        public Position Clone()
+        {
+            var clone = new Position(x, y);
+            return clone;
+        }
+
+        //returnera en string i följande format: (X, Y)
+        public override string ToString()
+        {
+            return $"({x},{y})";
+        }
+
+        //Skall jämföra 2 positioners avstånd från origo. p1.length>p2.length. Returnerar true om p1 ligger längst ifrånorigoReturnerar false annarsOm positionerna har samma längd från origo:Skall x•variablen jämföras istället
+        public static bool operator >(Position p1, Position p2)
+        {
+
+            if (p1.Length() > p2.Length())
+            {
+                return true;
+            }
+
+            if (p1.Length().Equals(p2.Length()))
+            { 
+                if (p1.X > p2.X)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator <(Position p1, Position p2)
+        {
+            if (p1.Length() < p2.Length())
+            {
+                return true;
+            }
+
+            if (p1.Length().Equals(p2.Length()))
+            {
+                if (p1.X < p2.X)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //Addera 2 punkter med varandra och returnerar en ny position
+        public static Position operator +(Position p1, Position p2)
+        {
+            return new Position(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        //Subtrahera 2 punkter med varandra och returnerar en ny position
+        public static Position operator -(Position p1, Position p2)
+        {
+            return new Position(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+        //Räkna ut avståndet mellan två punkter och returnera double
+        public static double operator %(Position p1, Position p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }      
+    }
+}
---
+++ Labb2/SortedPosList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Labb2
+{
+    //en klass som skall fungera som en lista för just positioner
+    public class SortedPosList
+    {
+        //instansvariabel. Den skall endast användas inuti klassen
+        private List<Position> positions = new List<Position>();
+
+        public SortedPosList()
+        {
+
+        }
+
+        //Skall returnera antalet positioner vi har i vår lista
+        public int Count()
+        {
+            return positions.Count;
+        }
+
+        //Skall lägga till en given position till vår lista
+        public void Add(Position pos)
+        {
+            for (int i = 0; i < positions.Count; i++)
+            {
+                if (pos < positions[i])
+                {
+                    positions.Insert(i, pos);
+                    return;
+                }
+            }
+            positions.Add(pos);
+        }
+        
+        //Skall ta bort positionen pos från listan. Returnerar true om pos fanns i listan och togs bort, och false annars.
+        public bool Remove(Position pos)
+        {
+            for (int i = 0; i < positions.Count; i++)
+            {
+                if (pos.Equals(positions[i]))
+                {
+                    positions.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //Skall returnera en ny instans av SortedPosList, som innehåller kopior av alla punkter som finns i den här listan
+        public SortedPosList Clone()
+        {
+            var cloneList = new SortedPosList();
+            for (int i = 0; i < positions.Count; i++)
+            {
+                cloneList.Add(positions[i].Clone());
+            }
+            return cloneList;
+        }
+
+        //returnera en ny lista(använd Clone), som innehåller alla positioner som befinner sig inom en given cirkel
+        public SortedPosList CircleContent(Position centerPos, double radius)
+        {
+            var circleList = Clone();
+            circleList.positions = circleList.positions.Where(position => IsInCircle(position, centerPos, radius)).ToList();
+
+            return circleList;
+        }
+
+        private bool IsInCircle(Position pos, Position centerPos, double radius)
+        {
+            return (Math.Pow(pos.X - centerPos.X, 2) + Math.Pow(pos.Y - centerPos.Y, 2)) < Math.Pow(radius, 2);
+        }
+
+        //returnera en ny lista (använd Clone), som är de två adderade listorna ihopslagna till en
+        public static SortedPosList operator +(SortedPosList sp1, SortedPosList sp2)
+        {
+            var addList = sp1.Clone();
+            for (int i = 0; i < sp2.Count(); i++)
+            {
+                addList.Add(sp2[i]);
+            }
+                return addList;
+        }
+
+        //Getter: ska hämta ett element på en given position i listan.
+        public Position this[int i] => positions[i];
+
+        public override string ToString()
+        {
+            return string.Join(", ", positions);
+        }
+    }
+}
+
